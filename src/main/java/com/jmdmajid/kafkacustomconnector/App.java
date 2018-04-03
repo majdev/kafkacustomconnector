dbd262029dddae7fa70f378c533811a612b5ce80
majdev <majid_2007j@yahoo.co.in>
2018-04-04 03:09:02 +0530

    Creating maven project structure

---
+++ src/main/java/com/jmdmajid/kafkacustomconnector/App.java
@@ -0,0 +1,13 @@
+package com.jmdmajid.kafkacustomconnector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
